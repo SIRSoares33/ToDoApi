44e7edf3c203ad18d7d3ecefcbd0654c518761e3
Gustavo Soares <sirgustavo4@gmail.com>
2026-02-17 10:14:45 -0300

    fix(UpdateUserHandler): Prevent duplicate email update

--- ToDo.Application/Features/Auth/Handlers/UpdateUserHandler.cs
+++ ToDo.Application/Features/Auth/Handlers/UpdateUserHandler.cs
@@ -9,17 +9,20 @@ namespace ToDo.Application.Features.Auth.Handlers;
 public class UpdateUserHandler(IUserRepository userRepository, IPasswordHasher<IAuthService> hasher) : IRequestHandler<UpdateUserCommand, Unit>
 {
     public async Task<Unit> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
-    { 
+    {
         var user = await userRepository.GetUserByIdAsync(request.id, cancellationToken)
             ?? throw new UnauthorizedAccessException("User id not found.");
 
-        if (!string.IsNullOrEmpty(request.dto.Password))
-            request.dto.Password = hasher.HashPassword(null, request.dto.Password);
+        request.dto.Password = request.dto.Password is not null ? hasher.HashPassword(null, request.dto.Password) : null;
+
+        if (!string.IsNullOrEmpty(request.dto.Email) && request.dto.Email != user.Email.Value &&
+            await userRepository.IsEmailRegisteredAsync(request.dto.Email, cancellationToken))
+            throw new UnauthorizedAccessException("Email is already registered.");
 
         user.Update(request.dto.Name, request.dto.Email, request.dto.Password, request.dto.Role);
 
-        await userRepository.UpdateUserAsync(user, cancellationToken); 
-        
+        await userRepository.UpdateUserAsync(user, cancellationToken);
+
         return Unit.Value;
     }
 }
