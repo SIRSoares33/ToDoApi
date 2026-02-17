f609ace2dab3500a5920067e875a017816fb1077
Gustavo Soares <sirgustavo4@gmail.com>
2026-02-17 10:15:01 -0300

    feat: Add UpdateUserValidation

---
+++ ToDo.Application/Features/Auth/Validations/UpdateUserValidation.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using ToDo.Application.DTOs;
+
+namespace ToDo.Application.Features.Auth.Validations;
+
+public class UpdateUserValidation : AbstractValidator<UpdateUserDto>
+{
+    public UpdateUserValidation()
+    {
+        
+        RuleFor(x => x.Name)
+            .MaximumLength(100).WithMessage("Name must not exceed 100 characters.")
+            .When(x => x.Name != null);
+
+        RuleFor(x => x.Email)
+            .EmailAddress().WithMessage("Invalid email format.")
+            .When(x => x.Name != null);
+
+        RuleFor(x => x.Password)
+            .MinimumLength(8)
+                .WithMessage("Password must be at least 8 characters long.")
+            .Must(p => p.Any(char.IsUpper))
+                .WithMessage("Password must contain at least one uppercase letter.")
+            .Must(p => p.Any(char.IsLower))
+                .WithMessage("Password must contain at least one lowercase letter.")
+            .Must(p => p.Any(char.IsDigit))
+                .WithMessage("Password must contain at least one digit.")
+            .Must(p => p.Any(ch => !char.IsLetterOrDigit(ch)))
+                .WithMessage("Password must contain at least one special character.")
+                .When(x => x.Name != null);
+    }
+}
