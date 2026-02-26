474abd0b76bb4ce0dd7fa8c0105f216f3d8ff304
Gustavo Soares <sirgustavo4@gmail.com>
2026-02-26 12:40:29 -0300

    feat: Creating ToDo Validations

---
+++ ToDo.Application/Features/Validations/ToDo/UpdateToDoValidation.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using ToDo.Application.DTOs;
+
+namespace ToDo.Application.Features.Validations.ToDo;
+
+public class UpdateToDoValidation : AbstractValidator<UpdateToDoDto>
+{
+    public UpdateToDoValidation()
+    {
+        When(x => !string.IsNullOrEmpty(x.Title), () =>
+        {
+            RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Title is required.")
+            .MaximumLength(100).WithMessage("Title must not exceed 100 characters.");
+        });
+        
+        RuleFor(x => x.Description)
+            .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+    }
+}
