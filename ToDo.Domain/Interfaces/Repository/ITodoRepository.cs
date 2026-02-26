fbab928de2462ac3e683445c7df1afcd3318e31b
Gustavo Soares <sirgustavo4@gmail.com>
2026-02-26 12:38:56 -0300

    feat: Creating ToDo Repository

---
+++ ToDo.Domain/Interfaces/Repository/ITodoRepository.cs
@@ -0,0 +1,47 @@
+﻿using ToDo.Domain.Entities;
+
+namespace ToDo.Domain.Interfaces.Repository;
+
+/// <summary>
+/// Defines the data access and manipulation methods for the <see cref="Todo"/> entity.
+/// </summary>
+public interface ITodoRepository
+{
+    /// <summary>
+    /// Adds a new task to the repository asynchronously.
+    /// </summary>
+    /// <param name="task">The task to add.</param>
+    Task AddAsync(Todo task, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Retrieves all tasks for a specific user asynchronously.
+    /// </summary>
+    /// <param name="userId">The identifier of the user whose tasks will be returned.</param>
+    /// <returns>A list of tasks associated with the user.</returns>
+    Task<List<Todo>> GetAllByUserIdAsync(Guid userId, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Retrieves a task by its identifier asynchronously.
+    /// </summary>
+    /// <param name="id">The identifier of the task.</param>
+    /// <returns>The corresponding task, or <c>null</c> if not found.</returns>
+    Task<Todo?> GetByIdAsync(Guid id, Guid userId, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Removes an existing task from the repository asynchronously.
+    /// </summary>
+    /// <param name="task">The task to remove.</param>
+    Task RemoveAsync(Todo task, CancellationToken cancellationToken);
+    /// <summary>
+    /// Remove all user todos.
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task RemoveAllAsync(IEnumerable<Todo> todos, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Updates the data of an existing task asynchronously.
+    /// </summary>
+    /// <param name="task">The task with updated data.</param>
+    Task UpdateAsync(Todo task, CancellationToken cancellationToken);
+}
---
+++ ToDo.Infrastructure/Repositories/TodoRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Domain.Entities;
+using ToDo.Domain.Interfaces.Repository;
+using ToDo.Infrastructure.Context;
+
+namespace ToDo.Infrastructure.Repositories;
+
+public class TodoRepository(AppDbContext context) : ITodoRepository
+{
+    #region ITodoRepository Methods
+    public async Task AddAsync(Todo task, CancellationToken cancellationToken)
+    {
+        await context.Todos.AddAsync(task, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task RemoveAsync(Todo task, CancellationToken cancellationToken)
+    {
+        context.Todos.Remove(task);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task UpdateAsync(Todo task, CancellationToken cancellationToken)
+    {
+        context.Todos.Update(task);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task RemoveAllAsync(IEnumerable<Todo> todos, CancellationToken cancellationToken)
+    {
+        context.Todos.RemoveRange(todos);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<Todo?> GetByIdAsync(Guid id, Guid userId, CancellationToken cancellationToken) 
+        => await context.Todos.AsNoTracking().Where(x => x.UserId == userId).FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+
+    public async Task<List<Todo>> GetAllByUserIdAsync(Guid userId, CancellationToken cancellationToken) 
+        => await context.Todos.AsNoTracking().Where(x => x.UserId == userId).ToListAsync(cancellationToken);
+    #endregion
+}
